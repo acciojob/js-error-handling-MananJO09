101ae43e49f5d628bfe6f195ae247bdfb1a03442
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-26 11:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-//your code here
-class OutOfRangeError extends Error{
-	constructor(arg) {
-		super.('Expression should only consist of integers and +-/* characters and not < arg >');
-		this.name='OutOfRangeError';
-	}
-	}
+class OutOfRangeError extends Error {
+  constructor(arg) {
+    super('Expression should only consist of integers and +-/* characters and not < arg >');
+    this.name = 'OutOfRangeError';
+  }
+}
+
 class InvalidExprError extends Error {
-	constructor(arg) {
-		super.('Expression should not have an invalid combination of expression');
-		this.name='InvalidExprError';
-	}
+  constructor(arg) {
+    super('Expression should not have an invalid combination of expression');
+    this.name = 'InvalidExprError';
+  }
 }
 
 function evalString(expression) {
-	try {
+  try {
     // Check for invalid operator combinations using regular expressions
     const invalidOperatorPattern = /(\+\+|--|\+\-|\-\+|\*\/|\/\*|\+\/|\/\+|\*\*|\/\/)/;
     if (invalidOperatorPattern.test(expression)) {
       throw new InvalidExprError();
     }
 
-	// Check if the expression starts with an invalid operator
-    const startsWithInvalidOperator = +,/,*;
+    // Check if the expression starts with an invalid operator
+    const startsWithInvalidOperator = /^[+/*]/;
     if (startsWithInvalidOperator.test(expression)) {
-      throw new SyntaxError('Expression should not starts with an invalid operator');
+      throw new SyntaxError('Expression should not start with an invalid operator');
     }
-	const endsWithInvalidOperator= +,/,*,-;
-	if(endsWithInvalidOperator.test(expression)){
-		throw new SyntaxError('Expression should not end with invalid operator');
-	}	
-	}
-	catch (error) {
+
+    // Check if the expression ends with an invalid operator
+    const endsWithInvalidOperator = /[+/*-]$/;
+    if (endsWithInvalidOperator.test(expression)) {
+      throw new SyntaxError('Expression should not end with an invalid operator');
+    }
+  } catch (error) {
     console.error(error);
   }
 }
